5a5fe02312c8b52101c85340a6ce78e8092016d2
direyesm <37608963+direyesm@users.noreply.github.com>
2020-07-29 22:59:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/grupal7/FirstFragment.java
@@ -0,0 +1,39 @@
+package com.example.grupal7;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+    private EditText txtIngres;
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.txtIngres = view.findViewById(R.id.txtIngres);
+        final Bundle data = new Bundle();
+        data.putString("data",this.txtIngres.getText().toString());
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment,data);
+            }
+        });
+    }
+}
